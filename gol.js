c482ae27bc918fc32084fd28baf043a7e97a599c
Sundeepk <kahlon.sundeep@gmail.com>
2019-03-13 23:04:22 +0000

    allow germs

--- gol.js
+++ gol.js
@@ -1,5 +1,3 @@
-
-
 let rows = 0;
 let cols = 0;
 let cellLength;
@@ -7,9 +5,11 @@ let rowLength;
 let cells;
 const cellSizePx = 5;
 let inside;
+let germsEnabled = false;
 
 function setup() {
-    createCanvas(800, 600);
+    let canvas = createCanvas(800, 600);
+    canvas.parent('sketch-holder');
     cols =  width / cellSizePx;
     rows =   height / cellSizePx;
     cells = create2DArray(rows, cols);
@@ -17,7 +17,11 @@ function setup() {
 
     for (let col = 0; col < cells.length; col++) {
         for (let row = 0; row < cells[col].length; row++) {
-            cells[col][row] = getRandomInt(2);
+            cells[col][row] = {
+                alive: getRandomInt(2),
+                isGerm: false,
+                aliveTurns: 0
+            }
         }
     }
     cellLength = cells.length;
@@ -26,55 +30,87 @@ function setup() {
 
 function draw() {
     background(1, 1, 1);
-
-    for (let i =0; i < cells.length; i++) {
-        for (let j = 0; j < cells[i].length; j++) {
-            if (cells[i][j] === 1) {
-                fill(inside);
-                rect(i * cellSizePx, j * cellSizePx, cellSizePx, cellSizePx);
-            }
-        }
-    }
-
-    let nextCells = create2DArray(rows, cols);
-
-    for (let c = 0; c < cells.length; c++) {
-        for (let r = 0; r < cells[c].length; r++) {
-
-            let nCount = countNeighbours(r, c);
-            let state = cells[c][r];
-
-            if (state === 0 && nCount === 3) {
-                nextCells[c][r] = 1;
-            } else if (state === 1 && (nCount < 2 || nCount > 3)) {
-                nextCells[c][r] = 0;
-            } else {
-                nextCells[c][r] = state;
-            }
-
-        }
-    }
-
-    cells = nextCells;
-
+    drawCells();
+    updateCells();
 }
 
-function getMinBoundingBox(col) {
-    return Math.max(0, col - 1);
+function updateCells() {
+  let nextCells = create2DArray(rows, cols);
+  for (let c = 0; c < cells.length; c++) {
+      for (let r = 0; r < cells[c].length; r++) {
+        if (germsEnabled) {
+          if (cells[c][r].aliveTurns >= 4) {
+              let cellToKill = getCellToKill(r, c);
+              nextCells[cellToKill.col][cellToKill.row] = { alive:0, aliveTurns: 0 }
+          }
+
+          if (cells[c][r].aliveTurns >= 5) {
+              nextCells[c][r] = { alive:0, aliveTurns: 0 }
+              continue;
+          }
+        }
+        nextCells[c][r] = getNewCellState(r, c);
+      }
+  }
+  cells = nextCells;
 }
 
-function getMaxBoundingBox(col) {
+function drawCells() {
+  for (let i =0; i < cells.length; i++) {
+      for (let j = 0; j < cells[i].length; j++) {
+         if (cells[i][j].alive === 1) {
+             fill(inside);
+             rect(i * cellSizePx, j * cellSizePx, cellSizePx, cellSizePx);
+          }
+      }
+  }
+}
+
+function getNewCellState(r, c) {
+  let nCount = countNeighbours(r, c);
+  let cell = cells[c][r];
+  if (cell.alive === 0 && nCount === 3) {
+      return { alive:1, aliveTurns: 0 };
+  } else if (cell.alive === 1 && (nCount < 2 || nCount > 3)) {
+      return { alive: 0, aliveTurns:0 };
+  } else {
+      return { ...cell, aliveTurns: cell.aliveTurns + 1 };
+  }
+}
+
+function getMin(val) {
+    return Math.max(0, val - 1);
+}
+
+function getMaxCol(col) {
     return Math.min(col + 1, cellLength - 1);
+}
+
+function getMaxRow(row) {
+    return Math.min(row + 1, rowLength - 1);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+function getCellToKill(row, col) {
+    return {
+        col: getRandomInt(getMin(col), getMaxCol(col)),
+        row: getRandomInt(getMin(row), getMaxCol(row))
+    }
 }
 
 function countNeighbours(row, col) {
     let nCount = 0;
-    for (let colOffset = getMinBoundingBox(col); colOffset <= getMaxBoundingBox(col); colOffset++) {
-        for (let rowOffset = getMinBoundingBox(row); rowOffset <= getMaxBoundingBox(row); rowOffset++) {
+    for (let colOffset = getMin(col); colOffset <= getMaxCol(col); colOffset++) {
+        for (let rowOffset = getMin(row); rowOffset <= getMaxRow(row); rowOffset++) {
             if (colOffset === col &&  rowOffset === row) {
                 continue;
             }
-            if (cells[colOffset][ rowOffset]) {
+            if (cells[colOffset][rowOffset].alive === 1) {
                 nCount++;
             }
         }
@@ -92,4 +128,9 @@ function create2DArray(rows, cols) {
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function toggleGerms() {
+  germsEnabled = !germsEnabled;
+  document.getElementById("germToggle").innerHTML = germsEnabled ? "Germs enabled" : "Germs disabled";
 }
